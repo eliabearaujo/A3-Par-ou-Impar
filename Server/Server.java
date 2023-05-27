c9f9c816e61c263bbe81580fb0746735f5d91fad
eliabearaujo <eliabe_trajano@hotmail.com>
2023-05-27 20:22:35 -0300

    Input da porta do servidor criado.

--- Server/Server.java
+++ Server/Server.java
@@ -3,5 +3,22 @@ package Server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
-public class Server {}
+// Classe que inicia o servidor e gerencia a conexão dos players
+
+public class Server {
+
+  public static void main(String[] args) throws IOException {
+    Scanner s = new Scanner(System.in);
+    //Usuario inputa qual porta do servidor estará disponivel.
+    System.out.print("Digite o número da porta do servidor: ");
+    int portaServer = s.nextInt();
+
+    ServerSocket serverSocket = new ServerSocket(portaServer);
+
+    while (true) {
+      Socket socketPlayer = serverSocket.accept();
+    }
+  }
+}
